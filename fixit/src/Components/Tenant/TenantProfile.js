61540405586145e8ce505528188ae9f73667cb38
Won Maung Thein <wonmaungthein@gmail.com>
2018-03-23 13:26:02 +0000

    npm run build

---
+++ fixit/src/Components/Tenant/TenantProfile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+
+export class TenantProfile extends Component {
+  render() {
+    return (
+      <div>
+        <h1>TenantProfile</h1>
+      </div>
+    );
+  }
+}
